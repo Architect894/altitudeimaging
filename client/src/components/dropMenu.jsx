4e09c1614668e817c010612521073bb0e0fdb4a1
Jacob Elliott <sendittojelliott@gmail.com>
2025-05-13 21:41:15 -0500

    updated videos and content folder

--- client/src/components/dropMenu.jsx
+++ client/src/components/dropMenu.jsx
@@ -44,7 +44,7 @@ export default function DropMenu() {
                         />
                     </Link>
                     <Link href="/videoedits" className="button-container py-1 px-3">
-                        Portfolio
+                        Content
                     </Link>
                     <Link
                         href="/contact"
@@ -92,7 +92,7 @@ export default function DropMenu() {
                         />
                     </Link>
                     <Link href="/videoedits" className="button-container py-1 px-3 text-white hover:text-yellow-400">
-                        Portfolio
+                        Content
                     </Link>
                     <Link
                         href="/contact"
