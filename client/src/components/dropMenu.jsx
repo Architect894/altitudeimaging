95f92ead9189a084dd4c2fa92f0b13a9b51d3810
Jacob Elliott <sendittojelliott@gmail.com>
2025-07-01 01:20:09 -0500

    labeled call to action differently

--- client/src/components/dropMenu.jsx
+++ client/src/components/dropMenu.jsx
@@ -60,7 +60,7 @@ export default function DropMenu() {
                         onClick={handleBookNowClick}
                         className="button-container py-1 px-4"
                     >
-                        Book Your Event
+                        Pricing
                     </button>
                 </div>
 
@@ -118,7 +118,7 @@ export default function DropMenu() {
                         onClick={handleBookNowClick}
                         className="button-container py-2 px-4 text-white"
                     >
-                        Book Your Event
+                        Pricing
                     </button>
                 </div>
             )}
