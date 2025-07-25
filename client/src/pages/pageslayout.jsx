138e67dc2c2c3095ca09b7ead882e020dda0eec1
Jacob Elliott <sendittojelliott@gmail.com>
2025-07-24 23:23:48 -0500

    added vercel analytics

--- client/src/pages/pageslayout.jsx
+++ client/src/pages/pageslayout.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import DropMenu from "../components/dropMenu";
 import Footer from "../components/footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function PagesLayout({ children }) {
     return (
